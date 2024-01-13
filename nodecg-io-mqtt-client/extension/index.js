766f9df8f6f382f7cb7430cf3ee5ca712762eeed
codeoverflow-org <>
2024-01-13 21:19:48 +0000

    Publish codeoverflow-org/nodecg-io@f903a48c1495b86a00f38a90f5c82a54f5208871

--- nodecg-io-mqtt-client/extension/index.js
+++ nodecg-io-mqtt-client/extension/index.js
@@ -14,7 +14,7 @@ class MQTTClientServiceClient {
                 this.client.end();
                 reject(err.message);
             });
-            this.client.on("connect", resolve);
+            this.client.on("connect", () => resolve());
             this.once = this.client.once;
             this.on = this.client.on;
             this.close = this.client.end;
